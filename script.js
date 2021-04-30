2e4f59888a765cc2c050671b81c1320951a0b256
Godreigneth <godreignethlevidibia@yahoo.com>
2021-04-30 21:04:24 +0000

    Created the Delete Bookmark Function

--- script.js
+++ script.js
@@ -86,3 +86,16 @@ function fetchBookmarks() {
   }
   buildBookmarks();
 }
+
+// Delete Bookmark
+function deleteBookmark(url) {
+  // Loop through the bookmarks array
+  bookmarks.forEach((bookmark, i) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(i, 1);
+    }
+  });
+  // Update bookmarks array in localStorage, re-populate DOM
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+}
